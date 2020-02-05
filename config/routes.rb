569c7b55017343815b8db5438dd43ad566f85353
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-05 17:14:37 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
+
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id', to: 'authors#post'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
 
 end
